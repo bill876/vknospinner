aa8f39d5e4e81239097410fc507327ab7baae7c9
Vladimir Evsyukov <thebill876@gmail.com>
2017-06-28 16:10:42 +0600

    add script

---
+++ vknospinner.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         VK Spinner Killer
+// @version      1.0.0
+// @description  Replaces fidget spinner image with proper loading spinner image
+// @author       bill876
+// @homepage     https://github.com/bill876/vknospinner
+// @supportURL   https://github.com/bill876/vknospinner/issues
+// @downloadURL  https://github.com/bill876/vknospinner/raw/master/vknospinner.user.js
+// @updateURL    https://github.com/bill876/vknospinner/raw/master/vknospinner.user.js
+// @include      /https?://(.*\.)?vk.com/
+// @resource icon1 https://i.imgur.com/QbN4Jyg.png
+// @run-at       document-end
+// @license      MIT
+// ==/UserScript==
+
+const s = document.createElement('style');
+s.innerHTML =
+`.fidget_spinner, .im-page--history.im-page--history_loading:before {
+	background: url('https://i.imgur.com/QbN4Jyg.png') !important;
+	background-size: contain !important;
+}`;
+document.body.appendChild(s);
