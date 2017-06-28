a8d610a2ce821b01561989e411c3345f41df5129
Vladimir Evsyukov <thebill876@gmail.com>
2017-06-28 17:00:03 +0600

    add meta

---
+++ vknospinner.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         VK No Spinner
+// @version      1.0.1
+// @description  Replaces fidget spinner image with proper loading spinner image
+// @author       bill876
+// @homepage     https://github.com/bill876/vknospinner
+// @supportURL   https://github.com/bill876/vknospinner/issues
+// @downloadURL  https://github.com/bill876/vknospinner/raw/master/vknospinner.user.js
+// @updateURL    https://github.com/bill876/vknospinner/raw/master/vknospinner.meta.js
+// @include      /https?://(.*\.)?vk.com/
+// @resource icon1 https://i.imgur.com/QbN4Jyg.png
+// @run-at       document-end
+// @license      MIT
+// ==/UserScript==
